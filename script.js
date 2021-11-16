b303bcf82d8ba0fd83eecbc8c4778a6f496fc278
bruna <busouza01@gmail.com>
2021-11-15 22:07:01 -0500

    verificado que as mensagens estão chegando do servidor

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let url = 'https://mock-api.driven.com.br/api/v4/uol/';
+let url = 'https://mock-api.driven.com.br/api/v4/uol';
 
-const namee = () => {
+const enterTheChat = () => {
     const user = document.querySelector('.enter-name').value;
     const userName = {
         name: user
     }
-
     console.log(userName);
 }
+
+const startChat = () => {
+    const promisse =axios.get(`${url}/messages`);
+    promisse.then(messagesChat);
+}
+
+const messagesChat = (response) => {
+    console.log(response.data);
+}
+
+startChat();
