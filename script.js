5bacffeeafb59bef63d446a89a0c57bb2ee9f33e
bruna <busouza01@gmail.com>
2021-11-15 22:49:35 -0500

    primeira filtragem de mensagens concluída

--- script.js
+++ script.js
@@ -15,6 +15,26 @@ const startChat = () => {
 
 const messagesChat = (response) => {
     console.log(response.data);
+
+    //ok, já sei que as mensagens estão chegando, e meu servirdor me retorna um array
+    //e eu quero percorrer esse array filtrando as mensagens, ok? ok! chama :v
+
+    const messagens = document.querySelector('.message-container');
+    messagens.innerHTML = ''; //quero que comece vazio :v
+    //agora vou percorrer esse array 
+    for(let i = 0 ; i < response.data.length ; i++) {
+        if (response.data[i].type === 'status') {
+            messagens.innerHTML += `
+            <li class="message-status">
+            <span class="time">(${response.data[i].time})</span>
+            <strong>${response.data[i].from}</strong>
+            <span> para </span>
+            <strong>${response.data[i].to}: </strong>
+            <span>${response.data[i].text}</span>
+          </li>
+            `
+        }
+    }
 }
 
 startChat();
